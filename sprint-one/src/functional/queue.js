34efb5eade5df0777934fcb7416ae7ab97551841
Doug Perez <perezcpt@gmail.com>
2020-11-18 14:51:58 -0800

    Complete Stacks of Functional Pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,25 @@
-var Queue = function() {
+var Queue = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
+  storage.start = 0;
+  storage.end = 0;
+  console.log(storage);
 
   // Implement the methods below
 
-  someInstance.enqueue = function(value) {
+  someInstance.enqueue = function (value) {
+    storage[storage.end] = value;
+    storage.end++;
   };
 
-  someInstance.dequeue = function() {
+  someInstance.dequeue = function () {
+    storage.end--;
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,29 @@
-var Stack = function() {
+var Stack = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
+  storage.start = 0;
+  storage.end = 0;
+
+
+
 
   // Implement the methods below
-  someInstance.push = function(value) {
+  someInstance.push = function (value) {
+    storage[storage.end] = value;
+    storage.end++;
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function () {
+    if (storage.end >= 1) {
+      storage.end--;
+      return storage[storage.end];
+    }
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    return storage.end - storage.start;
   };
 
   return someInstance;
