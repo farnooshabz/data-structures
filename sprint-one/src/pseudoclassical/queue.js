055f30789ae3336cfadc2cbe7a4b5ada04c3045a
farnooshabz <farabrishamkar@gmail.com>
2020-11-28 11:02:55 -0800

    sprintOne

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,36 +1,21 @@
-var Queue = function () {
+var Queue = function() {
   this.storage = {};
-  this.storage.start = 0;
-  this.storage.end = 0;
+  this.start = 0;
+  this.end = 0;
 };
 
-
-
-Queue.prototype.enqueue = function (value) {
-  this.storage[this.storage.end] = value;
-  this.storage.end++;
-  // console.log('this.storage after push', this.storage);
-  // console.log('this.storage.end', this.storage.end);
+Queue.prototype.enqueue = function(value) {
+  this.end ++;
+  this.storage[this.end] = value;
 };
-
-Queue.prototype.dequeue = function (value) {
-  if (this.storage.end - this.storage.start >= 1) {
-    //store the first item in var
-    var deletedItem = this.storage[this.storage.start];
-    //delete the first item
-    delete this.storage[this.storage.start];
-    //increase the start count to account for deleted
-    this.storage.start++;
-    //return the first item (which was deleted)
-    return deletedItem;
+Queue.prototype.dequeue = function () {
+  if (this.size()) {
+    this.start ++;
+    var poped = this.storage[this.start];
+    delete this.storage[this.start];
+    return poped;
   }
 };
-
-Queue.prototype.size = function () {
-  return this.storage.end - this.storage.start;
+Queue.prototype.size = function() {
+  return this.end - this.start;
 };
-
-new Queue();
-
-
-
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,29 +1,24 @@
-var Stack = function () {
-  //const instance = {}
+var Stack = function() {
+
   this.storage = {};
-  this.storage.start = 0;
-  this.storage.end = 0;
+  this.top = 0;
+
 
 
 };
-
-Stack.prototype.push = function (value) {
-  this.storage[this.storage.end] = value;
-  this.storage.end++;
+Stack.prototype.push = function(value) {
+  this.top ++;
+  this.storage[this.top] = value;
 };
-
-Stack.prototype.pop = function (value) {
-  if (this.storage.end >= 1) {
-    this.storage.end--;
-    return this.storage[this.storage.end];
+Stack.prototype.pop = function () {
+  if (this.size()) {
+    var poped = this.storage[this.top];
+    delete this.storage[this.top];
+    this.top --;
+    return poped;
   }
 };
-
-Stack.prototype.size = function () {
-  return this.storage.end - this.storage.start;
+Stack.prototype.size = function() {
+  return this.top;
 };
-
-new Stack();
-
-
 
