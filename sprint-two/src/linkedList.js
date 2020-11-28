c76947babc293f8a90fe989401860c733cbb004c
farnooshabz <farabrishamkar@gmail.com>
2020-11-27 21:50:07 -0800

    linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,59 +1,44 @@
-var LinkedList = function () {
+var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function (value) {
-    var newNode = Node(value);
-    if (list.head === null && list.tail === null) {
-      list.tail = newNode;
-      list.head = newNode;
+  list.addToTail = function(value) {
+    if (list.tail !== null) {
+      var lastOne = list.tail;
+      list.tail = Node(value);
+      lastOne.next = list.tail;
+    } else {
+      list.tail = Node(value);
+      list.head = list.tail;
     }
-    //work on syntax, the following lines will always execute after the if statement
-    var currentNode = list.tail;
-    currentNode.next = newNode;
-    list.tail = newNode;
 
-    //list.tail.next = newNode
-    //list.tail = list.tail.next
   };
 
-  list.removeHead = function () {
-    //console.log("list.head", list.head)
-    var firstNode = list.head;
-    delete list.head;
-    //delete removes key value pair
-    list.head = firstNode.next;
-    return firstNode.value;
+  list.removeHead = function() {
+    if (list.head !== null) {
+      var removed = list.head;
+      var nextOne = list.head.next;
+      delete list.head;
+      list.head = nextOne;
+      return removed.value;
+    }
+
   };
 
-  list.contains = function (target) {
-
-    //console.log("target", target)
-    //define a current var assigned to head
-    var element = list.head;
-    //while current in not null
-    //if (element !== null) {
-
-    //if(element) -Checks if its truthy or falsy, stick to strict comparisons for now
-    while (element !== null) {
-      //console.log("element", element)
-      //if current is equal to target
-      if (element.value === target) {
+  list.contains = function(target) {
+    for (var key in list) {
+      if (list[key].value === target) {
         return true;
       }
-      element = element.next;
-      //return true
     }
-    //if not found outside the while loop return false
     return false;
-
   };
 
   return list;
 };
 
-var Node = function (value) {
+var Node = function(value) {
   var node = {};
 
   node.value = value;
@@ -64,5 +49,5 @@ var Node = function (value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
- //Constant
+ linear
  */
